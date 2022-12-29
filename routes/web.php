fcdb7c547e9726962df287e03409411ceecf3d14
Takumi471 <takumi.higuchi27@gmail.com>
2022-12-30 04:58:09 +0900

    投稿の削除機能追加

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 Route::post('/posts', [PostController::class, 'store']);
 ?>
